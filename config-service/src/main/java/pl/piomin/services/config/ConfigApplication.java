7c82bc0a1f14607a38034b3eec1dd8c80f309363
rafurb <rafal.urbas@gmail.com>
2020-04-29 23:20:02 +0200

    jhgkyluyf

--- config-service/src/main/java/pl/piomin/services/config/ConfigApplication.java
+++ config-service/src/main/java/pl/piomin/services/config/ConfigApplication.java
@@ -2,10 +2,8 @@ package pl.piomin.services.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class ConfigApplication {
 
 	public static void main(String[] args) {
